206714e6e77d005ecab50fd5fd05765c0ae6d606
1 <115859190+CNnumber1@users.noreply.github.com>
2022-12-20 23:56:09 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,90 +1,25 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int x;
+int f();
+int main(void)
 {
+    int a = 1;
 
+    x = a;
+    a = f();
+    {
+        int b = 2;
+        b = a + b;
+        x = x + b;
+    }
+    printf("%d %d", a, x);              /*%d和%d之间有1个空格*/
+
+    return 0;
 }
 
-//
-// 输入字符型数字密码
-// {
-//	int ch = 0;
-//	while ((ch=getchar()) != EOF)
-//	{
-//		if (ch < '0' || ch>'9')
-//			continue;
-//		putchar(ch);
-//	}
-//	return 0;
-//}
+int f()
+{
+    int x = 4;
 
-//第二部分--设置密码
-// 使其可以一段一段读取缓冲区值
-// {
-//	int ch = 0;
-//	while ((ch=getchar()) != EOF)
-//	{
-//		putchar(ch);
-//	}
-//	return 0;
-//}
-
-//第二部分--getchar&&putchar均需要();
-// int char = getchar();
-// putchar(char);
-// {
-//	int ch = getchar();
-//
-//		putchar(ch);
-//		printf(" %c\n", ch);
-//	return 0;
-//}
-
-
-
-//第一部分--for循环
-// {
-//	int i = 1;
-//	for (i = 0;i<=10;i ++)
-//		printf("%d ", i);
-//	return 0;
-//}
-//第一部分--while循环
-// {
-//	int i = 0;
-//	while (i <= 10)
-//	{
-//		printf("%d\n", i);	
-//		i++;
-//	}
-//	return 0;
-//}
-
-//while -if
-// {
-//	int i = 0;
-//	while (i <= 10)
-//	{
-//		if (i == 5)	
-//			//break&&continue;break--永久跳出，continue--跳出本次。//
-//			printf("%d\n", i);
-//		i++;
-//	}
-//	return 0;
-//}
-
-//while
-// {
-//	int i = 0;
-//	while (i < 10)
-//	{
-//		printf("%d ", i);
-//		i++;
-//	}
-//	return 0;
-//}
-
-//{
-//	while (1)
-//		printf("hehe\n");
-//	return 0;
-//}
+    return x;
+}
