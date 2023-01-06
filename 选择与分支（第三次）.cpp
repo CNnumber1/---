08c73dff2d97e6b59294985a25a9e0b9aaec6640
1 <115859190+CNnumber1@users.noreply.github.com>
2023-01-06 23:39:54 +0800

    Add files via upload

---
+++ 选择与分支（第三次）.cpp
@@ -0,0 +1,107 @@
+#include<stdio.h>
+int main()
+
+
+
+//第三部分
+// {
+//	int n = 1;
+//	int i = 0;
+//	switch (n)
+//	{
+//	case 1:
+//		if (n == 1)
+//			printf("hehe\n");
+//	case 2:
+//		printf("工作日\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//{
+//	int day;
+//	scanf_s("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//		printf("工作日\n");
+//		break;
+//	case 5:
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}return 0;
+//}
+
+//{
+//	int day;
+//	scanf_s("%d", &day);
+//	switch (day) {
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//			break;
+//	case 3:
+//		printf("星期三\n");
+//			return 0;
+//	}
+//}
+
+
+
+
+//第二部分
+// while和if
+//{
+//	int i = 1;
+//	while (i < 100)
+//	{
+//		printf("%d ", i);
+//		i += 2;
+//	}
+//	return 0;
+//}
+
+//{
+//	int i = 1;
+//	while (i < 100)
+//	{
+//		if (i % 2 != 0)
+//			printf("%d ", i);
+//			i++;
+//	}
+//	return 0;
+//}
+
+//{
+//	int i = 1;
+//	while (i < 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
+//第一部分
+// if else
+//{
+// int age = 0;
+//	if (age<18)
+//		printf("");
+//	else
+//		printf("");
+//	return 0;
+//}
