e53f817f9b55c0424486983e9f2cbc77d71f34f4
1 <115859190+CNnumber1@users.noreply.github.com>
2023-01-14 15:17:14 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,44 +1,39 @@
 //#include<stdio.h>
-//int main(void)
-//{
-//    int x = 1, a = 0, b = 0;
 //
-//    switch (x)
+//
+//    struct stu {
+//        char num[10];
+//        float score[3];
+//    };
+//
+//    int main()
 //    {
-//    case 0:   b++;
-//    case 1:   a++;
-//    case 2:   a++; b++;
+//        struct stu s[3] = { { "20021",90,95,85},{"20022",95,80,75},{"20023",100,95,90} };
+//        struct stu* p = s;
+//        int i;
+//        float sum = 0;
+//
+//        for (i = 0; i < 3; i++) {
+//            sum = sum + p->score[i];
+//        }
+//        printf("%6.2f", sum);
+//
+//        return 0;
 //    }
-//    printf("a=%d,b=%d\n", a, b);
+//#include<stdio.h>
+//int x = 5, y = 7;
+//void swap()
+//{
+//    int z;
+//
+//    z = x;  x = y;  y = z;
+//}
+//int main(void)
+//{
+//    int x = 3, y = 8;
+//    swap();
+//    printf("%d,%d \n", x, y);
 //
 //    return 0;
-//}
-//#include <stdio.h>
-//int main(void)
-//{
-//    int i;
-//    for (i = 0; i < 3; i++)
-//        switch (i)
-//        {
-//        case 1: printf("%d", i);
-//        case 2: printf("%d", i);
-//        default: printf("%d", i);
-//        }
-//    return 0;
-//}
-//#include <stdio.h>
-//int main(void)
-//{
-//    int m, k = 0, s = 0;
-//    for (m = 1; m <= 4; m++) {
-//        switch (m % 4) {
-//        case 0:
-//        case 1: s += m; break;
-//        case 2:
-//        case 3: s -= m; break;
-//        }
-//        k += s;
-//    }
-//    printf("%d\n", k);
-//    return 0;
+//
 //}
