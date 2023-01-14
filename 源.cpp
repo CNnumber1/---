42eb9c221addf858bba638528379246950fcc49e
1 <115859190+CNnumber1@users.noreply.github.com>
2023-01-14 13:49:52 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,25 +1,35 @@
-#include <stdio.h>
-int x;
-int f();
-int main(void)
-{
-    int a = 1;
-
-    x = a;
-    a = f();
-    {
-        int b = 2;
-        b = a + b;
-        x = x + b;
-    }
-    printf("%d %d", a, x);              /*%d和%d之间有1个空格*/
-
-    return 0;
-}
-
-int f()
-{
-    int x = 4;
-
-    return x;
-}
+//#include<stdio.h>
+//int main(void)
+//{
+//    int  a = 2, b = -1, c = 2;
+//
+//    if (a < b)
+//        if (b < 0)
+//            c = 0;
+//        else  c++;
+//    printf("%d\n", c);
+//
+//    return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int a = 2, b = -1, c = 2;
+//    if (a)
+//        if (b < 0) c = 0;
+//        else c++;
+//    printf("%d\n", c);
+//    return 0;
+//}
+//#include <stdio.h>
+//int main(void)
+//{
+//    float x, y;
+//    scanf_s("%f", &x);
+//    if (x < 0.0)y = 0.0;
+//    else if ((x < 5.0) && (x != 2.0))y = 1.0 / (x + 2.0);
+//    else if (x < 10.0)y = 1.0 / x;
+//    else y = 10.0;
+//    printf("%f\n", y);
+//    return 0;
+//}
