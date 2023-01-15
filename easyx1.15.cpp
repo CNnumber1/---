eeeb86732a1e5445651b2fa2ae0813793e50eb20
1 <115859190+CNnumber1@users.noreply.github.com>
2023-01-15 15:19:16 +0800

    Add files via upload

---
+++ easyx1.15.cpp
@@ -0,0 +1,47 @@
+﻿#include<easyx.h>
+
+#include<stdio.h>
+//int main()
+//{
+//	initgraph(800, 600);
+//	setorigin(400, 300);
+//	setaspectratio(1, -1);
+//	line(100, -100, -100, 100);
+//	line(200, 200, -200, -200);
+//	getchar();
+//	closegraph();
+//	return 0;
+//}
+
+//for (int r = 10; r <= 300; r += 10)
+//{
+//	circle(0, 0, r);//圆
+//}
+
+//rectangle(-200, 100, 300, 200);//矩形
+
+//ellipse(-200, 100, 200, -100);//椭圆
+
+//roundrect(-200, 100, 200,
+//	        -100, 200, 100);//圆角矩形
+
+//#define PI 3.14
+//pie(-200, 100, 200,
+//	-100, 0, PI / 3);//扇形
+
+//#define PI 3.14
+//arc(-200, 100, 200,
+//	-100, 0, PI / 3);//圆弧
+
+
+int main()
+{
+	initgraph(800, 600);
+	setorigin(400, 300);
+	setaspectratio(1, -1);
+
+	getchar();
+	closegraph();
+	return 0;
+}
+
